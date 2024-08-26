d35a92dce05ed75cfdfa9b74f78f0e26bc240a6f
Diogo Kawaguchi Rosa <diogo.kawaguchi.rosa@gmail.com>
2024-08-26 14:04:51 -0300

    :truck: chore: arquivos movidos do model para db

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const db = require('./model/connection')
+const db = require('./db/connection')
 
 const PORT = 3000;
 
--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,6 @@ const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './model/app.db'
+    storage: './db/app.db'
 })
 module.exports = sequelize
