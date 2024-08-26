b301ebc54a380898387148e8abe16f433dbfad7b
Diogo Kawaguchi Rosa <diogo.kawaguchi.rosa@gmail.com>
2024-08-26 13:41:26 -0300

    :construction: wip: Iniciando a conexão com o banco de dados

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const db = require('./model/connection')
 
 const PORT = 3000;
 
@@ -7,6 +8,16 @@ app.listen(PORT, function(){
     console.log(`O express está rodando na porta ${PORT}`)
 })
 
+//Db Connection
+db.authenticate()
+.then(()=>{
+    console.log("Banco conectado")
+})
+.catch(err=>{
+    console.log("Ocorreu um erro ao conectar", err)
+})
+
+// Routes
 app.get('/', (req, res) => {
     res.send("Teste 1")
 })
