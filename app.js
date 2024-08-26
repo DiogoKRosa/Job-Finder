544b6a6e30705d528f09897f9b7f17908b17a86d
Diogo Kawaguchi Rosa <diogo.kawaguchi.rosa@gmail.com>
2024-08-26 14:39:27 -0300

    :construction: wip: Criando rota para adicionar registros na tabela jobs

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express();
 const db = require('./db/connection')
+const bodyParser = require('body-parser')
 
 const PORT = 3000;
 
 app.listen(PORT, function(){
     console.log(`O express está rodando na porta ${PORT}`)
 })
+
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}))
 
 //Db Connection
 db.authenticate()
@@ -21,3 +25,4 @@ db.authenticate()
 app.get('/', (req, res) => {
     res.send("Teste 1")
 })
+app.use('/jobs', require('./routes/jobs'))
