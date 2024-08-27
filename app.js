a2560175cbd446fd3e3be82e29184bc2e4c79b9e
Diogo Kawaguchi Rosa <diogo.kawaguchi.rosa@gmail.com>
2024-08-27 14:49:49 -0300

    :recycle: refactor: Movendo os arquivos html para templates do handlebar e passando os caminhos nas rotas

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express();
-const db = require('./db/connection')
-const bodyParser = require('body-parser')
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();;
+const db = require('./db/connection');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const PORT = 3000;
 
@@ -11,6 +13,15 @@ app.listen(PORT, function(){
 
 //Body Parser
 app.use(bodyParser.urlencoded({extended: false}))
+
+//handle bars
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Db Connection
 db.authenticate()
@@ -23,6 +34,6 @@ db.authenticate()
 
 // Routes
 app.get('/', (req, res) => {
-    res.send("Teste 1")
+    res.render('index')
 })
 app.use('/jobs', require('./routes/jobs'))
