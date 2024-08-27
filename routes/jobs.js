c5f9107de5014adb319e392c82b94290d5655647
Diogo Kawaguchi Rosa <diogo.kawaguchi.rosa@gmail.com>
2024-08-27 16:04:32 -0300

    :sparkles: feat: html da página view adicionado

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,17 +2,25 @@ const express = require('express')
 const router = express.Router()
 const Job = require('../models/Job')
 
-router.get('/test', (req,res)=>{
+router.get('/test', (req, res) => {
     res.send('Jobs deu certo')
 })
 
-router.get('/add', (req,res) => {
+//detalhe da vaga
+router.get('/view/:id', (req, res) => Job.findOne({
+    where: { id: req.params.id }
+    }).then(job => {
+    res.render('view', { job })
+    }).catch(err=>console.log(err))
+)
+
+router.get('/add', (req, res) => {
     res.render('add')
 })
 
 //add job via post
-router.post('/add', (req,res)=>{
-    let {title, salary, company, description, email, new_job} = req.body
+router.post('/add', (req, res) => {
+    let { title, salary, company, description, email, new_job } = req.body
 
     //insert
     Job.create({
@@ -23,8 +31,8 @@ router.post('/add', (req,res)=>{
         email,
         new_job
     })
-    .then(() => res.redirect('/'))
-    .catch(err => console.log(err))
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
 })
 
 module.exports = router
